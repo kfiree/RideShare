137be95a2e0e507cb2fbb205438b54439ec560f6
kfiree <kfirettinger@gmail.com>
2022-08-02 16:19:32 +0300

    add algo node

---
+++ src/main/java/app/controller/AlgoNode.java
@@ -0,0 +1,44 @@
+package app.controller;
+
+import app.model.Node;
+
+public class AlgoNode {
+    private Node node;
+    private double h = 0, g = 0, f = 0;
+
+    public AlgoNode(Node node) {
+        this.node = node;
+    }
+
+    public Node getNode() {
+        return node;
+    }
+
+    public void setNode(Node node) {
+        this.node = node;
+    }
+
+    public double getH() {
+        return h;
+    }
+
+    public void setH(double h) {
+        this.h = h;
+    }
+
+    public double getG() {
+        return g;
+    }
+
+    public void setG(double g) {
+        this.g = g;
+    }
+
+    public double getF() {
+        return f;
+    }
+
+    public void setF(double f) {
+        this.f = f;
+    }
+}
